f2f7bb5c66c3b35e72ebee4d23bc9af4e7c09525
coreen <coreeny@cs.washington.edu>
2017-06-15 21:56:52 -0700

    add simple example of bluebird promise

---
+++ simple.js
@@ -0,0 +1,28 @@
+var Promise = require('bluebird');
+
+var pizza = {
+	toppings: {
+		pepperoni: 8
+	},
+	cooked: false,
+	eaten: false
+};
+
+var cookPizza = new Promise(function(resolve, reject) {
+	setTimeout(function() {
+		pizza.cooked = true;
+		resolve(pizza);
+	}, 5000);
+});
+
+function eat(pizza) {
+	pizza.eaten = true;
+	console.log('eaten pizza', pizza);
+}
+
+cookPizza
+	.then(function(pizza) {
+		console.log('cooked pizza', pizza);
+		eat(pizza);
+	});
+
