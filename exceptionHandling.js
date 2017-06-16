8f6841cc05052006f62019fa4f81dca7cc6dc55f
coreen <coreeny@cs.washington.edu>
2017-06-15 23:19:39 -0700

    exception handling example chain

---
+++ exceptionHandling.js
@@ -0,0 +1,32 @@
+var Promise = require('bluebird');
+
+var pizza = {
+	toppings: {
+		pepperoni: 8
+	},
+	cooked: 'undercooked',
+	eaten: false
+};
+var cookPizza = new Promise(function(resolve, reject) {
+	setTimeout(function() {
+		pizza.cooked = 'overcooked';
+		reject("burnt pizza");
+	}, 5000);
+});
+function eat(pizza) {
+	pizza.eaten = true;
+	console.log('eaten:', pizza);
+}
+
+// main
+console.log('before', pizza);
+cookPizza
+	.then(function(pizza) {
+		console.log('cooked', pizza);
+		eat(pizza);
+	})
+	.catch(function(exception) {
+		console.log("exception:", exception);
+		console.log("after:", pizza);
+	});
+
